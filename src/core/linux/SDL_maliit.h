919cda97f1aec381ed292e55140ade065bd45031
Peter G. (nephros) <sailfish@nephros.org>
2025-12-09 15:02:14 +0100

    Fix Copyright date

--- src/core/linux/SDL_maliit.h
+++ src/core/linux/SDL_maliit.h
@@ -1,7 +1,7 @@
 /*
   Simple DirectMedia Layer
   Copyright (C) 1997-2024 Sam Lantinga <slouken@libsdl.org>
-  Copyright (C) 2024 Peter G. <sailfish@nephros.org>
+  Copyright (C) 2025 Peter G. <sailfish@nephros.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
